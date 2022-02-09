c56ec63aa596f9073ed3da06d88e95e45b68a7b9
sanket.mhatre1 <sanket.mhatre@trell.in>
2022-02-10 00:13:16 +0530

    update

--- app/users/users.py
+++ app/users/users.py
@@ -1,3 +1,5 @@
+from sre_constants import SUCCESS
+from tkinter import E
 from flask import Blueprint, request, jsonify, make_response
 from app import jwt, mongo
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -5,8 +7,15 @@ from app.utils.helper import *
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required
 import datetime
+import os
+from werkzeug.utils import secure_filename
+from firebase_admin import credentials, initialize_app, storage
+
+cred = credentials.Certificate("garage-4a443-cebfff850cea.json")
+initialize_app(cred, {'storageBucket': 'garage-4a443.appspot.com'})
 
 user_collection = mongo.db.users
+parking_space_collection = mongo.db.parkingSpaces
 
 user = Blueprint('user', __name__)
 
@@ -43,7 +52,7 @@ def register():
     user_info['isPhoneVerified'] = False
 
     access_token = create_access_token(identity=user_info['uid'])
-    user_collection.insert(user_info)
+    user_collection.insert_one(user_info)
     user_data = user_info
     user_data.pop("_id")
 
@@ -77,4 +86,115 @@ def login():
         return make_response('Login using Google', 400)
     
 
+ALLOWED_IMAGE_EXTENSIONS = set(['png','jpg','jpeg','gif'])
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
+
+@user.route('/uploadImage', methods=['POST'])
+def uploadImage():
+    if 'file' not in request.files:
+        return  make_response('Missing Files', 400)
+    file = request.files['file']
+    if file.filename == '':
+        return make_response('Missing files', 400)
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join("image_dump",filename)
+        file.save(filepath)
+        bucket = storage.bucket()
+        blob = bucket.blob(filepath)
+        blob.upload_from_filename(filepath)
+        blob.make_public()
+        return jsonify(msg="Successfully uploaded image", public_url=blob.public_url), 200
+    else:
+        return make_response('Image type not supported', 400)
+
+
+
+@user.route('/addParkingSpace', methods=['POST'])
+def add_parking_space():
+    if not request.is_json:
+        return make_response('Missing JSON in request', 400)
+    
+    payload = request.json
+  
+    params = ['coords', 'images', 'addressText', 'uid', 'completeAddress']
+    for param in params:
+        if not param in payload.keys():
+            make_response("Missing {}".format(param)), 400
+    
+    payload['pid'] = "PS" + str(random_with_N_digits(8))
+    
+    parking_space_collection.insert_one(payload)
+
+    return make_response('Successfully added Parking Space', 200)
+
+
+@user.route('/getAllParkingSpaces', methods=['GET', 'POST'])
+def get_all_parking_spaces():
+    if request.is_json:
+        payload = request.json
+        uid = payload.get("uid")
+        if not uid:
+            return make_response('Missing uid'), 400
+
+        parking_spaces_list = list(parking_space_collection.find({"uid": uid}))
+        for space in parking_spaces_list:
+            space.pop("_id")
+        
+        return jsonify(parking_spaces_list=parking_spaces_list, success=True), 200
+
+    else:
+        parking_spaces_list = list(parking_space_collection.find())
+        for space in parking_spaces_list:
+            space.pop("_id")
+        
+        return jsonify(parking_spaces_list=parking_spaces_list, success=True), 200
+
+
+@user.route('/getParkingSpacebyID', methods=['POST'])
+def get_parking_space():
+    if not request.is_json:
+        return make_response('Missing JSON in request', 400)
+    
+    payload = request.json
+    pid = payload.get("pid")
+    if not pid:
+        return make_response('Missing pid', 400)
+    
+    parking_space = parking_space_collection.find_one({"pid": pid})
+    parking_space.pop("_id")
+
+    return jsonify(parking_space=parking_space, success=True), 200
+
+
+@user.route('/markParkingSpaceActive', methods=['POST'])
+def mark_parking_space_active():
+    if not request.is_json:
+        return make_response('Missing JSON in request', 400)
+    
+    payload = request.json
+    params = ["pid", "parking_slots", "rent_per_hour", "avilable_from", "avilable_to"]
+    for param in params:
+        if not param in payload.keys():
+            make_response("Missing {}".format(param)), 400
+
+   
+    parking_space = parking_space_collection.find_one({"pid": payload.get("pid")})
+    
+    if not parking_space:
+        return make_response('Invalid pid', 400)
+
+    newValues = { "$set": {
+        "parking_slots": payload.get("parking_slots"),
+        "rent_per_hour": payload.get("rent_per_hour"), 
+        "available_from": payload.get("available_from"),
+        "available_to": payload.get("available_to"),
+        "isActive": 1
+    }}
+    
+    parking_space_collection.update_one({"pid": payload.get("pid")},newValues)
+ 
+    return jsonify(msg="successfull updated", pid=payload.get("pid"), success=True), 200
